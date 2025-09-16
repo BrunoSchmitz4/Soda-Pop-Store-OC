48de38ea1e56fba75d8c1ea01e77852510e52cf4
Bruno Schmitz <85579710+BrunoSchmitz4@users.noreply.github.com>
2025-09-16 14:53:22 -0300

    Criação da Página Carrinho

---
+++ src/components/Titulo/index.jsx
@@ -0,0 +1,8 @@
+
+function Titulo() {
+    return (
+        <h1>Bem vindo(a) Soda-Pop Store</h1>
+    )
+}
+
+export default Titulo;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import Titulo from "../../components/Titulo";
 
 function Home() {
     return (
         <>
             <Header />
+            <Titulo /> 
         </>
     )
 }
